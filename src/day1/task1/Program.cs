a0f81fe9d2b9c244ad9007ddae113f322309d924
Antonín Procházka <prochazka@tocna.info>
2025-12-01 09:28:02 +0100

    Day 1 Task 1

---
+++ src/day1/task1/Program.cs
@@ -0,0 +1,39 @@
+﻿var lines = File.ReadLines(GetInputFilePath(
+    //"sinput.txt"
+    "input.txt"
+));
+
+var increments = lines.Select(line =>
+{
+    var multiplier = line[0] switch
+    {
+        'L' => -1,
+        'R' => 1,
+        _ => throw new InvalidOperationException($"Unexpected value '{line[0]}'")
+    };
+
+    var value = long.Parse(line[1..]);
+
+    return multiplier * value;
+});
+
+var counter = new RotaryCounter(50);
+var zeroCount = counter.Count == 0 ? 1L : 0L;
+
+foreach (var increment in increments)
+{
+    counter.Increment(increment);
+
+    if (counter.Count == 0)
+    {
+        zeroCount++;
+    }
+}
+
+Console.WriteLine(zeroCount);
+
+static string GetInputFilePath(string inputFileName, [System.Runtime.CompilerServices.CallerFilePath] string? sourceCodePath = null)
+{
+    var sourceCodeDirectory = Path.GetDirectoryName(sourceCodePath);
+    return Path.Combine(sourceCodeDirectory!, "input", inputFileName);
+}
---
+++ src/day1/task1/RotaryCounter.cs
@@ -0,0 +1,21 @@
+﻿internal class RotaryCounter
+{
+    public long Count { get; private set; }
+
+    public RotaryCounter(long count)
+    {
+        Count = count;
+    }
+
+    public void Increment(long value)
+    {
+        var newCount = Count + (value % 100);
+        
+        Count = newCount switch
+        {
+            > 99 => newCount - 100,
+            < 0 => newCount + 100,
+            _ => newCount
+        };
+    }
+}
