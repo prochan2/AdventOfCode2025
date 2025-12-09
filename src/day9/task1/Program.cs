6d27010bb70f0c4b53b6f54bf1dff648c7f68ece
Antonín Procházka <prochazka@tocna.info>
2025-12-09 15:51:41 +0100

    Day 9 Task 1

---
+++ src/day9/task1/Program.cs
@@ -0,0 +1,72 @@
+﻿var redTiles = new LinkedList<Tile>();
+
+await foreach (var line in File.ReadLinesAsync(GetInputFilePath(
+    //"sinput.txt"
+    "input.txt"
+)))
+{
+    var parts = line.Split(',');
+    
+    var tile = new Tile(
+        long.Parse(parts[0]),
+        long.Parse(parts[1])
+    );
+
+    redTiles.AddLast(tile);
+}
+
+Rectangle? largestRectangle = null;
+
+foreach (var corner1 in redTiles)
+{
+    foreach (var corner2 in redTiles)
+    {
+        if (corner1 == corner2)
+        {
+            continue;
+        }
+
+        var rectangle = new Rectangle(corner1, corner2);
+
+        if (largestRectangle == null || rectangle.Area > largestRectangle.Area)
+        {
+            largestRectangle = rectangle;
+        }
+    }
+}
+
+Console.WriteLine(largestRectangle!.Area);
+
+static string GetInputFilePath(string inputFileName, [System.Runtime.CompilerServices.CallerFilePath] string? sourceCodePath = null)
+{
+    var sourceCodeDirectory = Path.GetDirectoryName(sourceCodePath);
+    return Path.Combine(sourceCodeDirectory!, "input", inputFileName);
+}
+
+class Tile
+{
+    public long X { get; }
+
+    public long Y { get; }
+
+    public Tile(long x, long y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+class Rectangle
+{
+    public Tile Corner1 { get; }
+
+    public Tile Corner2 { get; }
+
+    public Rectangle(Tile corner1, Tile corner2)
+    {
+        Corner1 = corner1;
+        Corner2 = corner2;
+    }
+
+    public long Area => Math.Abs((Corner2.X - Corner1.X + 1) * (Corner2.Y - Corner1.Y + 1));
+}
