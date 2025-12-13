98aa1b0672160b663e7e20be30f9fa9dee915e97
Antonín Procházka <prochazka@tocna.info>
2025-12-13 21:06:07 +0100

    Day 9 Task 2 improvement in progress

--- src/day9/task2/Program.cs
+++ src/day9/task2/Program.cs
@@ -1,167 +1,31 @@
 ﻿using Microsoft.VisualBasic;
 
-var redTiles = new LinkedList<Tile>();
+var redTiles = new LinkedList<Position>();
 var floor = new Floor();
 
 await foreach (var line in File.ReadLinesAsync(GetInputFilePath(
-    //"sinput.txt"
-    "input.txt"
+    "sinput.txt"
+    //"input.txt"
 )))
 {
     var parts = line.Split(',');
     
-    var tile = new Tile(
+    var redTile = new Position(
         long.Parse(parts[0]),
-        long.Parse(parts[1]),
-        TileColor.Red
+        long.Parse(parts[1])
     );
 
-    floor.Place(tile);
-    redTiles.AddLast(tile);
+    floor.Place(new(redTile.X, redTile.Y));
+    redTiles.AddLast(redTile);
 }
 
-// Fill lines
-for (var y = 0L; y < floor.Height; y++)
-{
-    Tile? firstRedTile = null;
-    Tile? secondRedTile = null;
-
-    for (var x = 0L; x < floor.Width; x++)
-    {
-        var currentTile = floor[x, y];
-
-        if (currentTile == null || currentTile.Color != TileColor.Red)
-        {
-            continue;
-        }
-
-        if (firstRedTile == null)
-        {
-            firstRedTile = currentTile;
-        }
-        else
-        {
-            secondRedTile = currentTile;
-            break;
-        }
-    }
-
-    if (firstRedTile == null || secondRedTile == null)
-    {
-        continue;
-    }
-
-    for (var x = firstRedTile.X + 1; x < secondRedTile.X; x++)
-    {
-        floor.Place(new Tile(x, y, TileColor.Green));
-    }
-}
-
-// Fill columns
-for (var x = 0L; x < floor.Width; x++)
-{
-    Tile? firstRedTile = null;
-    Tile? secondRedTile = null;
-
-    for (var y = 0L; y < floor.Height; y++)
-    {
-        var currentTile = floor[x, y];
-
-        if (currentTile == null || currentTile.Color != TileColor.Red)
-        {
-            continue;
-        }
-
-        if (firstRedTile == null)
-        {
-            firstRedTile = currentTile;
-        }
-        else
-        {
-            secondRedTile = currentTile;
-            break;
-        }
-    }
-
-    if (firstRedTile == null || secondRedTile == null)
-    {
-        continue;
-    }
-
-    for (var y = firstRedTile.Y + 1; y < secondRedTile.Y; y++)
-    {
-        floor.Place(new(x, y, TileColor.Green));
-    }
-}
-
-// Fill area
-for (var y = 0L; y < floor.Height; y++)
-{
-    Tile? firstColoredTile = null;
-    Tile? lastColoredTile = null;
-
-    for (var x = 0L; x < floor.Width; x++)
-    {
-        var currentTile = floor[x, y];
-
-        if (currentTile != null && currentTile.Color != TileColor.Other)
-        {
-            firstColoredTile = currentTile;
-            break;
-        }
-    }
-
-    if (firstColoredTile == null)
-    {
-        continue;
-    }
-
-    for (var x = floor.Width - 1; x > firstColoredTile.X; x--)
-    {
-        var currentTile = floor[x, y];
-
-        if (currentTile != null && currentTile.Color != TileColor.Other)
-        {
-            lastColoredTile = currentTile;
-            break;
-        }
-    }
-
-    if (lastColoredTile == null)
-    {
-        continue;
-    }
-
-    for (var x = firstColoredTile.X + 1; x < lastColoredTile.X; x++)
-    {
-        if (floor[x, y] == null)
-        {
-            floor.Place(new(x, y, TileColor.Green));
-        }
-    }
-}
-
-// Print
-for (var y = 0L; y < floor.Height; y++)
-{
-    for (var x = 0L; x < floor.Width; x++)
-    {
-        Console.Write(floor[x, y] switch
-        {
-            { Color: TileColor.Red } => '#',
-            { Color: TileColor.Green } => 'X',
-            _ => '.'
-        });
-    }
-
-    Console.WriteLine();
-}
+floor.FillTheBlanks();
 
 // Find largest filled
 var incompleteRectangles = new HashSet<Rectangle>();
 Rectangle? largestRectangle = null;
 
-for (var i = floor.Width * floor.Height; i > 0; i++)
+for (var i = redTiles.Count * redTiles.Count; i > 0; i++)
 {
     largestRectangle = null;
 
@@ -196,67 +60,15 @@ for (var i = floor.Width * floor.Height; i > 0; i++)
 
     Console.WriteLine();
 
-    for (var y = 0L; y < floor.Height; y++)
-    {
-        for (var x = 0L; x < floor.Width; x++)
-        {
-            var tile = floor[x, y];
-
-            Console.Write(tile switch
-            {
-                _ when tile == largestRectangle.Corner1 || tile == largestRectangle.Corner2 => 'O',
-                { Color: TileColor.Red } => '#',
-                { Color: TileColor.Green } => 'X',
-                _ => '.'
-            });
-        }
-
-        Console.WriteLine();
-    }
-
-    bool isRectangleFullyColored = true;
-
-    var x1 = largestRectangle.Corner1.X;
-    var y1 = largestRectangle.Corner1.Y;
-    var x2 = largestRectangle.Corner2.X;
-    var y2 = largestRectangle.Corner2.Y;
-
-    if (x1 > x2)
-    {
-        var tmp = x2;
-        x2 = x1;
-        x1 = tmp;
-    }
-
-    if (y1 > y2)
-    {
-        var tmp = y2;
-        y2 = y1;
-        y1 = tmp;
-    }
-
-    for (var y = y1; y < y2; y++)
-    {
-        for (var x = x1; x < x2; x++)
-        {
-            var currentTile = floor[x, y];
-
-            if (currentTile == null || currentTile.Color == TileColor.Other)
-            {
-                isRectangleFullyColored = false;
-                incompleteRectangles.Add(largestRectangle);
-                break;
-            }
-        }
-
-        if (!isRectangleFullyColored)
-        {
-            break;
-        }
-    }
+    bool isRectangleFullyColored =
+        floor[largestRectangle.Corner1]
+        && floor[largestRectangle.Corner2]
+        && floor[largestRectangle.Corner3]
+        && floor[largestRectangle.Corner4];
 
     if (!isRectangleFullyColored)
     {
+        incompleteRectangles.Add(largestRectangle);
         continue;
     }
 
@@ -271,52 +83,104 @@ static string GetInputFilePath(string inputFileName, [System.Runtime.CompilerSer
     return Path.Combine(sourceCodeDirectory!, "input", inputFileName);
 }
 
-enum TileColor
-{
-    Red,
-    Green,
-    Other
-}
-
-class Tile
-{
-    public long X { get; }
-
-    public long Y { get; }
-
-    public TileColor Color { get; set; }
-
-    public Tile(long x, long y, TileColor color)
-    {
-        X = x;
-        Y = y;
-        Color = color;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        return obj is Tile other && this.X == other.X && this.Y == other.Y && this.Color == other.Color;
-    }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(this.X, this.Y, this.Color);
-    }
-}
-
+/// <summary>
+///   X ----->
+/// Y C1 .. C2
+/// |  .      .
+/// V C4 .. C3
+/// </summary>
 class Rectangle
 {
-    public Tile Corner1 { get; }
+    public Position Corner1 { get; }
 
-    public Tile Corner2 { get; }
+    public Position Corner2 { get; }
 
-    public Rectangle(Tile corner1, Tile corner2)
+    public Position Corner3 { get; }
+
+    public Position Corner4 { get; }
+
+    public Rectangle(Position cornerA, Position cornerB)
     {
-        Corner1 = corner1;
-        Corner2 = corner2;
+        //   X -------->
+        // Y C1:A . C2:O
+        // | .         .
+        // V C4:O . C3:B
+        //
+        // OR
+        //
+        // Y X -------------->
+        // V C1=C4:A . C2=C3:B
+        if (cornerA.X < cornerB.X && cornerA.Y <= cornerB.Y)
+        {
+            Corner1 = cornerA;
+            Corner2 = new(cornerB.X, cornerA.Y);
+            Corner3 = cornerB;
+            Corner4 = new(cornerA.X, cornerB.Y);
+            return;
+        }
+
+        //   X -------->
+        // Y C1:O . C2:A
+        // | .         .
+        // V C4:B . C3:O
+        //
+        // OR
+        //
+        //   X----->
+        // Y C1=C2:A
+        // | .
+        // V C4=C3:B
+        if (cornerA.X >= cornerB.X && cornerA.Y < cornerB.Y)
+        {
+            Corner1 = new(cornerB.X, cornerA.Y);
+            Corner2 = cornerA;
+            Corner3 = new(cornerA.X, cornerB.Y);
+            Corner4 = cornerB;
+            return;
+        }
+
+        //   X -------->
+        // Y C1:B . C2:O
+        // | .         .
+        // V C4:O . C3:A
+        //
+        // OR
+        //
+        // Y X -------------->
+        // V C1=C4:B . C2=C3:A
+        if (cornerA.X > cornerB.X && cornerA.Y >= cornerB.Y)
+        {
+            Corner1 = cornerB;
+            Corner2 = new(cornerA.X, cornerB.Y);
+            Corner3 = cornerA;
+            Corner4 = new(cornerB.X, cornerA.Y);
+            return;
+        }
+
+        //   X -------->
+        // Y C1:O . C2:B
+        // | .         .
+        // V C4:A . C3:O
+        //
+        // OR
+        //
+        //   X----->
+        // Y C1=C2:B
+        // | .
+        // V C4=C3:A
+        if (cornerA.X <= cornerB.X && cornerA.Y > cornerB.Y)
+        {
+            Corner1 = new(cornerA.X, cornerB.Y);
+            Corner2 = cornerB;
+            Corner3 = new(cornerB.X, cornerA.Y);
+            Corner4 = cornerA;
+            return;
+        }
+
+        throw new InvalidOperationException();
     }
 
-    public long Area => Math.Abs((Corner2.X - Corner1.X + 1) * (Corner2.Y - Corner1.Y + 1));
+    public long Area => (Corner3.X - Corner1.X + 1) * (Corner3.Y - Corner1.Y + 1);
 
     public override bool Equals(object? obj)
     {
@@ -329,20 +193,155 @@ class Rectangle
     }
 }
 
+record Position(long X, long Y)
+{
+    public override int GetHashCode() => HashCode.Combine(X, Y);
+}
+
+class Row
+{
+    public Position First { get; private set; }
+
+    public Position Last { get; private set; }
+
+    public Row(Position first, Position last)
+    {
+        if (first.Y != last.Y)
+        {
+            throw new InvalidOperationException();
+        }
+
+        First = first;
+        Last = last;
+    }
+
+    public bool IsColored(long x) => x >= this.First.X && x <= this.Last.X;
+
+    public bool Place(Position position)
+    {
+        if (position.Y != this.First.Y)
+        {
+            throw new InvalidOperationException();
+        }
+
+        if (position.X < this.First.X)
+        {
+            this.First = position;
+            return true;
+        }
+
+        if (position.X > this.Last.X)
+        {
+            this.Last = position;
+            return true;
+        }
+
+        return false;
+    }
+}
+
+class Column
+{
+    public Position First { get; private set; }
+
+    public Position Last { get; private set; }
+
+    public Column(Position first, Position last)
+    {
+        if (first.X != last.X)
+        {
+            throw new InvalidOperationException();
+        }
+
+        First = first;
+        Last = last;
+    }
+
+    public bool IsColored(long y) => y >= this.First.Y && y <= this.Last.Y;
+
+    public bool Place(Position position)
+    {
+        if (position.X != this.First.X)
+        {
+            throw new InvalidOperationException();
+        }
+
+        if (position.Y < this.First.Y)
+        {
+            this.First = position;
+            return true;
+        }
+        
+        if (position.Y > this.Last.Y)
+        {
+            this.Last = position;
+            return true;
+        }
+
+        return false;
+    }
+}
+
 class Floor
 {
-    private readonly Dictionary<(long X, long Y), Tile?> _tiles = new();
+    private readonly Dictionary<long, Row> _rows = new();
 
-    public long Width { get; private set; }
+    private readonly Dictionary<long, Column> _columns = new();
 
-    public long Height { get; private set; }
+    public bool this[Position position] =>
+        (_rows.TryGetValue(position.Y, out var row) && row.IsColored(position.X))
+        || (_columns.TryGetValue(position.X, out var column) && column.IsColored(position.Y));
 
-    public Tile? this[long x, long y] => _tiles.TryGetValue((x, y), out var tile) ? tile : null;
+    public bool Place(long x, long y) => this.Place(new(x, y));
 
-    public void Place(Tile tile)
+    public bool Place(Position position)
     {
-        _tiles[(tile.X, tile.Y)] = tile;
-        Width = Math.Max(Width, tile.X + 1);
-        Height = Math.Max(Height, tile.Y + 1);
+        bool anyChange = false;
+
+        if (_rows.TryGetValue(position.Y, out var row))
+        {
+            anyChange |= row.Place(position);
+        }
+        else
+        {
+            _rows[position.Y] = new(position, position);
+            anyChange = true;
+        }
+
+        if (_columns.TryGetValue(position.X, out var column))
+        {
+            anyChange |= column.Place(position);
+        }
+        else
+        {
+            _columns[position.X] = new(position, position);
+            anyChange = true;
+        }
+
+        return anyChange;
+    }
+
+    public void FillTheBlanks()
+    {
+        var iterations = 0L;
+        bool anyChange;
+
+        do
+        {
+            anyChange = false;
+
+            foreach (var row in _rows.Values.ToArray())
+            {
+                foreach (var column in _columns.Values.ToArray())
+                {
+                    anyChange |= this.Place(row.First.X, column.First.Y);
+                    anyChange |= this.Place(row.First.X, column.Last.Y);
+                    anyChange |= this.Place(row.Last.X, column.First.Y);
+                    anyChange |= this.Place(row.Last.X, column.Last.Y);
+                }
+            }
+
+            Console.WriteLine(iterations++);
+        } while (anyChange);
     }
 }
